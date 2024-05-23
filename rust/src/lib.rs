2ba6045be3df8a9b5ea6dda15bf785f8143e1f4a
Sidharth Rao <sidharthmrao@gmail.com>
2024-05-22 20:12:54 -0400

    Fixed extension errors, successfully created a base extension.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use godot::prelude::*;
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
