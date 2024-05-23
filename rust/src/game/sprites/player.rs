072c76b51c2cfd9137aa0868a229eb3d662f44c1
Sidharth Rao <sidharthmrao@gmail.com>
2024-05-22 20:59:48 -0400

    Physics.

--- rust/src/game/sprites/player.rs
+++ rust/src/game/sprites/player.rs
@@ -1,6 +1,6 @@
-use godot::prelude::*;
-use godot::engine::Sprite2D;
 use godot::engine::ISprite2D;
+use godot::engine::Sprite2D;
+use godot::prelude::*;
 
 #[derive(GodotClass)]
 #[class(base=Sprite2D)]
@@ -11,7 +11,6 @@ pub struct Player {
     base: Base<Sprite2D>
 }
 
-
 #[godot_api]
 impl ISprite2D for Player {
     fn init(base: Base<Sprite2D>) -> Self {
@@ -20,7 +19,12 @@ impl ISprite2D for Player {
         Self {
             speed: 400.0,
             angular_speed: std::f64::consts::PI,
-            base,
+            base
         }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
     }
 }
