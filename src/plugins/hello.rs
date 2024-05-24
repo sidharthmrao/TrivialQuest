778905d95103db8b7a8c11a370f253d562141fe2
Sidharth Rao <sidharthmrao@gmail.com>
2024-05-24 02:39:30 -0400

    Added timers.

--- src/plugins/hello.rs
+++ src/plugins/hello.rs
@@ -1,14 +1,16 @@
-use crate::systems::hello::hello_world;
-use crate::systems::people::{add_people, greet_people, update_people};
+use crate::systems::people::{add_people, greet_people, GreetTimer, update_people, UpdateTimer};
 use bevy::prelude::*;
 
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, add_people).add_systems(
+        app
+            .insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .insert_resource(UpdateTimer(Timer::from_seconds(3.0, TimerMode::Repeating)))
+            .add_systems(Startup, add_people).add_systems(
             Update,
-            ((update_people, greet_people).chain(), hello_world)
+            (update_people, greet_people).chain()
         );
 
         println!("Built HelloPlugin.");
